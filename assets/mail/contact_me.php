afd7f4b09524da6373802756ce90c1c3047eeb42
MY-Sabil <sabilsabil121@gmail.com>
2020-11-19 19:07:03 +0600

    probytes site v0.71b

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -12,8 +12,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = "sabilsabil121@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$subject = "Website Contact Form - Probytes:  $name";
+$body = "You have received a new message from your website probytes.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@probytes.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
